d5d6de564828b0dbb3c1e703dcff80606bbb5667
Gresham Dave <greyashdave@gmail.com>
2025-01-01 22:44:59 +0530

    mobile responsive work desc card 2

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -20,7 +20,7 @@ const Footer = () => {
             <div className="t"><h1>+-+-+-&nbsp;&nbsp;</h1><p>Last Updated 19-12-2024</p><h1>&nbsp;&nbsp;-+-+-+</h1></div>
             <div className="t"><h1>+-+-+-&nbsp;&nbsp;</h1><p>&copy; 2024 Grey.</p><h1>&nbsp;&nbsp;-+-+-+</h1></div>
             <div className="t"><h1>+-+-+-&nbsp;&nbsp;</h1><p>Gresham Dave | Portfolio</p><h1>&nbsp;&nbsp;-+-+-+</h1></div>
-            <div className="t"><h1>+-+-+-&nbsp;&nbsp;</h1><p>Last Updated 19-12-2024</p><h1>&nbsp;&nbsp;-+-+-+</h1></div>
+            <div className="t"><h1>+-+-+-&nbsp;&nbsp;</h1><p>Last Updated 01-01-2024</p><h1>&nbsp;&nbsp;-+-+-+</h1></div>
           </div>
         </marquee>
       </div>
